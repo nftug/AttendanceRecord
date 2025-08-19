acaa908459f8a496ab4d0e6ade3a4f56fe22f2ba
V-nyang <l64906947+nftug@users.noreply.github.com>
2025-08-20 00:06:19 +0900

    feat: Add Mac environment settings

--- publish_mac_app.sh
+++ publish_mac_app.sh
@@ -11,27 +11,7 @@ mkdir -p "$APP_BUNDLE/Contents/Resources"
 cp -r $BIN_PATH/* "$APP_BUNDLE/Contents/MacOS"
 rm -r "$APP_BUNDLE/Contents/MacOS/$APP_NAME.dSYM"
 
-cat > "$APP_BUNDLE/Contents/Info.plist" <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<plist version="1.0">
-<dict>
-  <key>CFBundleName</key>
-  <string>$APP_NAME</string>
-  <key>CFBundleIdentifier</key>
-  <string>com.example.$APP_NAME</string>
-  <key>CFBundleVersion</key>
-  <string>1.0</string>
-  <key>CFBundleExecutable</key>
-  <string>$APP_NAME</string>
-  <key>CFBundlePackageType</key>
-  <string>APPL</string>
-  <key>LSMinimumSystemVersion</key>
-  <string>10.13</string>
-  <key>CFBundleInfoDictionaryVersion</key>
-  <string>6.0</string>
-</dict>
-</plist>
-EOF
+cp "./$PROJECT_NAME/Info.plist" "$APP_BUNDLE/Contents/"
 
 chmod +x "$APP_BUNDLE/Contents/MacOS/$APP_NAME"
 xattr -dr com.apple.quarantine "$APP_BUNDLE"
